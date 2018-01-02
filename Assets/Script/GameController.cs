78588027c3a0affda223af6727b45b87782f7e43
yishenyao <5428037@qq.com>
2018-01-02 20:59:25 +0800

    底层

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DrawCells(int wCount,int hCount){
+		//画出格子
+	}
+
+	void DrawGridNum(){
+		//画出格子旁边的关卡数字
+	}
+
+	void LoadMission(int MissionID){
+		//读取关卡
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
