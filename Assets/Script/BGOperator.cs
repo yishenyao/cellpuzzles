a4055fec34006161fc7ea14f7d0d248e198f83ea
gabreil <zhangz053623@126.com>
2018-01-05 19:25:28 +0800

    可玩一局Demo

---
+++ Assets/Script/BGOperator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGOperator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnMouseDown(){
+		Vector3 pos;
+		if (Input.touchCount > 0) 
+		{
+			pos = Input.GetTouch(0).position;
+		} 
+		else 
+		{
+			pos = Input.mousePosition;
+		}
+		GameController.gameController.OnClickGrid (Camera.main.ScreenToWorldPoint(pos));
+	}
+}
--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+enum enColor
+{
+	white = 0,
+	red,
+	blue,
+	magenta,
+	green
+};
+
 public class GameController : MonoBehaviour {
 	static Color[] colors = {
 		Color.white,
@@ -12,25 +21,43 @@ public class GameController : MonoBehaviour {
 		Color.green,
 	};
 
+	public static GameController gameController;
+
+	public int nCurColor;
+
 	int nRowCount;					// 关卡行数
 	int nColCount;					// 关卡列数
 	int[,] MissionData;				// 关卡数据，例：[1, 2]为第一行第二列的颜色号，方向为左上到右下
 	int[,] AnswerData;				// 玩家解答数据
+	GameObject[,] ColorGrids;		// 玩家点出的色块
 
 	List<Vector2>[] RowNums;		// 从关卡数据分析出的每行的色块数量
 	List<Vector2>[] ColumnNums;		// 从关卡数据分析出的每列的色块数量
 
-	Vector3 leftUpPos;
+	Vector3 leftUpPos;				// 左上角坐标
+	float	fGridLength;			// 小格长度
+
+	bool	bWin = false;
+
+	public Text txtWin;
 
 	// Use this for initialization
 	void Start () {
+		txtWin.enabled = false;
+		nCurColor = 0;
 		leftUpPos = new Vector3 (-2, 1, 0);
+		fGridLength = 0.5f;
 		LoadMission (0);
 		DrawCells (nRowCount, nColCount);
 		DrawGridNum ();
 		// DrawRightAnswer ();
 	}
-	
+
+	//获得实例
+	void Awake(){
+		gameController = this;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
@@ -39,12 +66,8 @@ public class GameController : MonoBehaviour {
 	// 画出背景格子
 	void DrawCells(int wCount, int hCount){
 		Vector3 startPos = leftUpPos;
-		DrawGrid (startPos, 0.5f, wCount, hCount, 0.03f);
-		DrawGrid (startPos, 2.5f, wCount / 5, hCount / 5, 0.06f);
-	}
-
-	void DrawCellButton(){
-		//画出用户点击的格子对象
+		DrawGrid (startPos, fGridLength, wCount, hCount, 0.03f);
+		DrawGrid (startPos, fGridLength * 5, wCount / 5, hCount / 5, 0.06f);
 	}
 
 	// 画出格子旁边的关卡数字
@@ -98,9 +121,11 @@ public class GameController : MonoBehaviour {
 	// 读取关卡
 	void LoadMission(int MissionID){
 		// 随机布置一个关卡
-		nRowCount = 10;
-		nColCount = 10;
+		nRowCount = 5;
+		nColCount = 5;
 		MissionData = new int[nRowCount, nColCount];
+		AnswerData = new int[nRowCount, nColCount];
+		InitColorGrids ();
 		for (int i = 0; i < nRowCount; i++) {
 			for (int j = 0; j < nRowCount; j++) {
 				MissionData [i, j] = UnityEngine.Random.Range (0, 4);
@@ -145,12 +170,14 @@ public class GameController : MonoBehaviour {
 	}
 
 	bool CompareAnswer(){
-		//比较用户答案与正确答案
-		return false;
-	}
-
-	public void UpdateAnswer(int w,int h){
-		//更新用户答案
+		for (int x = 0; x < nRowCount; x++) {
+			for (int y = 0; y < nRowCount; y++) {
+				if (MissionData [x, y] != AnswerData [x, y]) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	// 画出正确答案
@@ -165,16 +192,40 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	// 添加一个图块
-	void AddOneColor(Vector3 pos, Color color) {
-		GameObject mUICanvas = GameObject.Find("Canvas");
-		GameObject ColorImage = (GameObject)Resources.Load("Prefabs/ColorSquare");  
-		ColorImage = GameObject.Instantiate(ColorImage);
-		ColorImage.transform.SetParent (mUICanvas.transform);
-		ColorImage.transform.position = pos;
-		ColorImage.transform.localScale = new Vector3(0.25f, 0.25f, 1);
-		Image img = ColorImage.GetComponent<Image> ();
-		img.color = color;
+	// 逻辑坐标添加一个图块
+	void AddOneColor(Vector2 logicPos, Color color) {
+		
+	}
+
+	void InitColorGrids() {
+		ColorGrids = new GameObject[nRowCount, nColCount];
+		for (int x = 0; x < nRowCount; x++) {
+			for (int y = 0; y < nRowCount; y++) {
+				Vector3 gridPos = leftUpPos + new Vector3 (0.25f + 0.5f * x, -0.25f - 0.5f * y, 0);
+				GameObject mUICanvas = GameObject.Find("Canvas");
+				GameObject ColorImage = (GameObject)Resources.Load("Prefabs/ColorSquare");  
+				ColorGrids[x,y] = GameObject.Instantiate(ColorImage);
+				ColorGrids[x,y].transform.SetParent (mUICanvas.transform);
+				ColorGrids[x,y].transform.position = gridPos;
+				ColorGrids[x,y].transform.localScale = new Vector3(0.25f, 0.25f, 1);
+				Image img = ColorGrids[x,y].GetComponent<Image> ();
+				img.color = Color.white;
+				ColorGrids [x, y].SetActive (false);
+			}
+		}
+	}
+
+	void ChangeGridColor(Vector2 LogicPos, int nColor) {
+		int nRow = (int)LogicPos.x;
+		int nCol = (int)LogicPos.y;
+		AnswerData [nCol, nRow] = nColor;
+		if (nColor == (int)enColor.white) {
+			ColorGrids [nRow, nCol].SetActive (false);
+		} else {
+			ColorGrids [nRow, nCol].SetActive (true);
+			Image img = ColorGrids [nRow, nCol].GetComponent<Image> ();
+			img.color = colors [nColor];
+		}
 	}
 
 	// 绘制格子, startPos为坐上角坐标
@@ -207,6 +258,42 @@ public class GameController : MonoBehaviour {
 			lineRenderer.SetPositions (pos);
 			lineRenderer.startWidth = fLintWidth;
 			lineRenderer.endWidth = fLintWidth;
+		}
+	}
+
+	public void OnWhite() {
+		nCurColor = (int)enColor.white;
+	}
+
+	public void OnRed() {
+		nCurColor = (int)enColor.red;
+	}
+
+	public void OnBlue() {
+		nCurColor = (int)enColor.blue;
+	}
+
+	public void OnMagenta() {
+		nCurColor = (int)enColor.magenta;
+	}
+
+	public void OnGreen() {
+		nCurColor = (int)enColor.green;
+	}
+
+	public void OnClickGrid(Vector3 pos) {
+		if (bWin) {
+			return;
+		}
+		Vector3 gridPos = (pos - leftUpPos) / fGridLength;
+		int nRow = (int)Mathf.Abs (gridPos.x);
+		int nCol = (int)Mathf.Abs (gridPos.y);
+		Vector2 gridLogicPos = new Vector2 (nRow, nCol);
+		Debug.Log (gridLogicPos);
+		ChangeGridColor(gridLogicPos, nCurColor);
+		if (CompareAnswer () == true) {
+			txtWin.enabled = true;
+			bWin = true;
 		}
 	}
 }
